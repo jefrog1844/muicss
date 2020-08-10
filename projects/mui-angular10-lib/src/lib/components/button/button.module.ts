2070f2821a0e4c1fa91dd2644ad5324f5ad56277
Jeff Rogers <jefrog1844@gmail.com>
2020-08-10 17:22:39 -0500

    import directivesmodule

--- projects/mui-angular10-lib/src/lib/components/button/button.module.ts
+++ projects/mui-angular10-lib/src/lib/components/button/button.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MuiDirectivesModule} from '../../directives/directives.module';
 import { MuiButtonComponent } from './button.component';
 
 @NgModule({
   declarations: [MuiButtonComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MuiDirectivesModule
   ],
   exports: [MuiButtonComponent]
 })
