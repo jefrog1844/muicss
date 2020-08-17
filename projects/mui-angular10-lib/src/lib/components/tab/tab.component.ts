7946239cdbb80c258a7f9acba3c5fdbd2493c7d7
Jeff Rogers <jefrog1844@gmail.com>
2020-08-17 16:34:25 -0500

    first draft of tabs

--- projects/mui-angular10-lib/src/lib/components/tab/tab.component.ts
+++ projects/mui-angular10-lib/src/lib/components/tab/tab.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mui-tab',
   template: `
-    <p>
-      tab works!
-    </p>
+  <div class="mui-tabs__pane" [ngClass]="{'mui--is-active': tabId === selectedId}"><ng-content></ng-content></div>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class MuiTabComponent implements OnInit {
+
+  @Input()
+  label: string;
+
+  @Input()
+  selectedId: any;
+
+  @Input()
+  tabId: any;
+
+  @Output() muiSelect = new EventEmitter<any>();
+  @Output() muiDeselect = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(index: any) {
+    this.muiSelect.emit(index);
+  }
+
+  onDeselect(index: any) {
+    this.muiDeselect.emit(index);
+  }
 }
--- projects/mui-angular10-lib/src/lib/components/tabs/tabs.component.ts
+++ projects/mui-angular10-lib/src/lib/components/tabs/tabs.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChildren, QueryList, Input } from '@angular/core';
+import { MuiTabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'mui-tabs',
   template: `
-    <p>
-      tabs works!
-    </p>
+  <ul class="mui-tabs__bar" [ngClass]="{'mui-tabs__bar--justified': justified}">
+    <li *ngFor="let tab of tabs; let i = index" [ngClass]="{'mui--is-active': $index === selectedId}">
+      <a (click)="onClick($index)">{{tab.label}}</a>
+    </li>
+  </ul>
   `,
   styles: [
   ]
 })
 export class MuiTabsComponent implements OnInit {
 
+  @Input()
+  justified?: boolean = false;
+
+  @ContentChildren(MuiTabComponent)
+  tabs: QueryList<MuiTabComponent>;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick(index: number) {
+
+  }
 }
