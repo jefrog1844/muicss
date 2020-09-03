f5f09132ffb9c2660a7709a8eb31ab225e06e238
Jeff Rogers <jefrog1844@gmail.com>
2020-09-02 22:20:39 -0500

    fix ripple

--- projects/mui-angular10-lib/button/button.module.ts
+++ projects/mui-angular10-lib/button/button.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-//import {MuiDirectivesModule} from '../directives/directives.module';
 import { MuiButtonComponent } from './button.component';
 import {MuiDirectivesModule} from 'mui-angular10-lib/directives';
 
 @NgModule({
   declarations: [MuiButtonComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MuiDirectivesModule
   ],
   exports: [MuiButtonComponent]
 })
