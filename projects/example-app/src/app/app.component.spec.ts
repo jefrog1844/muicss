830a974a8d8b196cb4516a16510133588c80b2b7
Jeff Rogers <jefrog1844@gmail.com>
2020-09-06 11:05:47 -0500

    rename mui-angular10-app to example-app

--- projects/example-app/src/app/app.component.spec.ts
+++ projects/example-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mui-angular10-app'`, () => {
+  it(`should have as title 'example-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mui-angular10-app');
+    expect(app.title).toEqual('example-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mui-angular10-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('example-app app is running!');
   });
 });
--- projects/example-app/src/app/app.component.ts
+++ projects/example-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mui-angular10-app';
+  title = 'example-app';
 }
